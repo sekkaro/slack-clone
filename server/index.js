a8da7696e93665799791a2ac16e1fd79f12dff23
Seob Lee이준섭Jun <junseoblee96@gmail.com>
2021-05-13 16:54:39 +0900

    Setting up GraphQL Server with Node.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+
+const main = async () => {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  app.use((req, res) => {
+    res.status(200);
+    res.send("Hello!");
+    res.end();
+  });
+
+  await new Promise((resolve) => app.listen({ port: 8080 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`);
+  return { server, app };
+};
+
+main();
